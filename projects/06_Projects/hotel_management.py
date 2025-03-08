5ac4f4774e531102a8b3802e473be4b6ce70b05d
ILearnToCodeHooray <134112193+ILearnToCodeHooray@users.noreply.github.com>
2025-03-08 19:27:19 +0000

    day18

--- projects/06_Projects/hotel_management.py
+++ projects/06_Projects/hotel_management.py
@@ -1,5 +1,6 @@
 '''checking in or out, charging guests for rooms, multiple room and multiple night plus one new feature
 i'm going to get program working, worry about meeting requirements after'''
+from tkinter import Tk, simpledialog, messagebox
 #Variable to check if check in should run again
 done = 0
 #Room number
@@ -14,44 +15,44 @@ def individual_check_in(done, number, price):
     cost = price
     while done == 0:
         try:
-            number = int(input("Hello, what room are you checking into? Rooms 1-50 are $100 a night and 51-100 are $200 a night. "))
+            number = simpledialog.askinteger("room number", "Hello, what room are you checking into? \nRooms 1-50 are $100 a night and 51-100 are $200 a night. ")
             if number > 50 and number <=100:
-                num_of_nights = int(input("How many nights will you be staying?"))
+                num_of_nights = int(simpledialog.askinteger("night number", "How many nights will you be staying?"))
                 cost = 200 * num_of_nights
                 price_word = str(cost)
-                print("That will add $" + price_word + " to your total.")
+                messagebox.showinfo("Total", "That will add $" + price_word + " to your total.")
                 total_price.append(cost)
                 done = 1
 
             elif number <= 50 and number > 0:
-                num_of_nights = int(input("How many nights will you be staying?"))
+                num_of_nights = simpledialog.askinteger("night number", "How many nights will you be staying?")
                 cost = 100 * num_of_nights
                 price_word = str(cost)
-                print("This will add $" + price_word + " to your total.")
+                messagebox.showinfo("Total", "This will add $" + price_word + " to your total.")
                 total_price.append(cost)
                 done = 1
 
             else:
-                print("Please enter a valid room number")
+                messagebox.showinfo("Valid number", "Please enter a valid room number")
 
         except ValueError:
-                print("Please enter a valid room number")
+                messagebox.showinfo("Valid number", "Please enter a valid room number")
 
 def check_in():
-    individual_check_in(done, number, cost)
-    extra_rooms = input("Will you be checking into any other rooms? (y/n)")
+    extra_rooms = simpledialog.askstring("extra rooms", "Will you be checking into any other rooms? (y/n)")
     if extra_rooms == ("y"):
         individual_check_in(done, number, cost)
     elif extra_rooms == ("n"):
         for i in total_price:
             total = total + total_price(i)
-        print("your total is " + total)
+        messagebox.showinfo("New total", "your total is " + total)
 
 
     
 def in_or_out():
+    in_question = simpledialog.askstring("extra rooms", "Are you checking in? (y/n)")
     if in_question == ("y"):
+        individual_check_in(done, number, cost)
         check_in()
-in_question = input("Are you checking in? (y/n)")
 while True:
     in_or_out()
